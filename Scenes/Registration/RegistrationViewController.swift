d885afbfba5edb6ad13cfb48e91ee0890adc337d
Mariammeskhi <mariam.meskhi18b@geolab.edu.ge>
2025-02-12 16:47:38 +0400

    Initial commit

---
+++ Scenes/Registration/RegistrationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationViewController.swift
+//  Final Project
+//
+//  Created by mariam meskhi on 12.02.25.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
