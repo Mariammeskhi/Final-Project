d848f69de36c791f188ca149dd41b07d5c4391ba
Mariammeskhi <mariam.meskhi18b@geolab.edu.ge>
2025-02-13 17:28:10 +0400

    set up registration design

--- Scenes/Registration/RegistrationViewController.swift
+++ Scenes/Registration/RegistrationViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let fields = [nameField, userNameField, emailField, passwordField]
+           for field in fields {
+               field?.layer.cornerRadius = 15
+               field?.layer.borderWidth = 1
+               field?.layer.borderColor = UIColor.lightGray.cgColor
+               field?.clipsToBounds = true
+           }
+        
+        registerButton.layer.cornerRadius = 38
     }
 
-
+    
+    @IBAction func didTapRegister(_ sender: UIButton) {
+    }
+    
 }
 
