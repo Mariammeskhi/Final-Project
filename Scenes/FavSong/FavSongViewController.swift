62ffa5ef2bae3e7b9ea574a0b785a955d460e746
Mariammeskhi <mariam.meskhi18b@geolab.edu.ge>
2025-02-19 19:57:12 +0400

    set up fav song

--- Scenes/FavSong/FavSongViewController.swift
+++ Scenes/FavSong/FavSongViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import CoreData
+
 class FavSongViewController: UIViewController {
     
     
@@ -20,6 +22,11 @@ class FavSongViewController: UIViewController {
     
     @IBOutlet weak var removeFromFavorites: UIButton!
     
+        var songTitleText: String?
+        var songAuthorText: String?
+        var songImageURL: String?
+        var managedObjectContext: NSManagedObjectContext?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -32,10 +39,44 @@ class FavSongViewController: UIViewController {
            }
         
         removeFromFavorites.layer.cornerRadius = 38
+        
+        populateData()
     }
     
+    private func populateData() {
+           songTitle.text = songTitleText
+           songAuthor.text = songAuthorText
+
+           if let imageURL = songImageURL, let url = URL(string: imageURL) {
+               DispatchQueue.global().async {
+                   if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                       DispatchQueue.main.async {
+                           self.songImage.image = image
+                       }
+                   }
+               }
+           }
+       }
+
 
     @IBAction func didTapRemoveFromFavorites(_ sender: Any) {
+        removeFromCoreData()
     }
     
-}
+    private func removeFromCoreData() {
+          guard let context = managedObjectContext else { return }
+          let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "FavoriteSong")
+          fetchRequest.predicate = NSPredicate(format: "title == %@", songTitleText ?? "")
+
+          do {
+              let results = try context.fetch(fetchRequest)
+              if let objectToDelete = results.first {
+                  context.delete(objectToDelete)
+                  try context.save()
+                  navigationController?.popViewController(animated: true)
+              }
+          } catch {
+              print("Failed to delete the song from favorites: \(error)")
+          }
+      }
+  }
